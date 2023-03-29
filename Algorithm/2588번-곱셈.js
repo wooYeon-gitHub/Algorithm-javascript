8513a48de13bdb69bb20977c2b0e34e38f256113
KATO <125425594+wooYeon-gitHub@users.noreply.github.com>
2023-03-29 08:21:48 +0000

    2588번 - 곱셈문제

---
+++ Algorithm/2588번-곱셈.js
@@ -0,0 +1,18 @@
+// 입력 데이터가 텍스트 파일 형태로 주어지는 경우, 파일 시스템 모듈을 사용
+// 예를 들어 /dev/stdin파일에 적힌 텍스트를 읽어오는 경우, 다음과 같이 코드를 작성한다.
+// 기능: 전체 텍스트를 읽어 온 뒤에, 줄바꿈 기호를 기준으로 구분하여 리스트로 변환하기
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let a = input[0];
+let b = input[1];
+
+x_1 = b[2]; // 일의 자리
+x_2 = b[1]; // 십의 자리
+x_3 = b[0]; // 백의 자리
+
+console.log(Number(a) * Number(x_1));
+console.log(Number(a) * Number(x_2));
+console.log(Number(a) * Number(x_3));
+console.log(Number(a) * Number(b));
