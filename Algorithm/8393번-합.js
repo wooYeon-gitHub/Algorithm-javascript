4b3af6c37072e7a67397d487e2a8e8bc688a1093
KATO <125425594+wooYeon-gitHub@users.noreply.github.com>
2023-03-30 08:43:00 +0000

    8393번 합

---
+++ Algorithm/8393번-합.js
@@ -0,0 +1,18 @@
+// 입력 데이터가 텍스트 파일 형태로 주어지는 경우, 파일 시스템 모듈을 사용
+// 예를 들어 /dev/stdin파일에 적힌 텍스트를 읽어오는 경우, 다음과 같이 코드를 작성한다.
+// 기능: 전체 텍스트를 읽어 온 뒤에, 줄바꿈 기호를 기준으로 구분하여 리스트로 변환하기
+
+// 이 문제의 시간 복잡도는 0(N)이다.
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+// 문자열을 수로 변환할 때 parseInt에 비하여 Number의 속도가 더 빠르게 동작
+let n = Number(input[0]);
+let summary = 0;
+
+for (let i = 1; i <= n; i++) {
+  summary += i;
+}
+
+console.log(summary);
